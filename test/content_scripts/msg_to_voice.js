2bd55f7da349c73d8bf53e2544cb415eb88bcc07
taksh <takshtax@gmail.com>
2021-01-30 20:24:46 +0530

    added message to voice functionality

--- test/content_scripts/msg_to_voice.js
+++ test/content_scripts/msg_to_voice.js
@@ -1,1 +1,38 @@
 console.log("Content Script loaded");
+var synth = window.speechSynthesis;
+
+let textContainer = document.querySelector("div[aria-live='polite']");
+
+const voices = synth.getVoices();
+
+var selectedVoice;
+
+voices.forEach(voice => {
+    if(voice.name.includes("en-US") || voice.lang.includes("en-US")) 
+    selectedVoice = voice;
+});
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: false  };
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) {
+    if (mutationsList.length > 1) {
+        console.log("Length increased");
+    }
+    var utterThis = new SpeechSynthesisUtterance(textContainer.innerText);
+    utterThis.voice = selectedVoice;
+    utterThis.pitch = 1;
+    utterThis.rate =1;
+    synth.speak(utterThis);
+    console.log(textContainer.innerText);
+};
+
+
+
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(textContainer, config);
