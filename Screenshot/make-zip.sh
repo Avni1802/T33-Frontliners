7c4d4201ccb75c91b9392f5e16b3e3928767267e
Avni1802 <avani1agrawal@gmail.com>
2021-01-31 15:26:57 +0530

    Merged all extensions

--- Screenshot/make-zip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-V=$(cat extension/manifest.json | jq -Mr .version)
-rm -f "one-click-screenshot-$V.zip"
-cd extension
-zip -r "../one-click-screenshot-$V.zip" . -x '*.git*' -x '*.DS_Store' -x '*Thumbs.db'
